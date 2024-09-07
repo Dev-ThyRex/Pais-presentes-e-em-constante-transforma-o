d41f21bf4ac373137588cce6326bc6a7b09e554a
Thyago R Reis <pr.thyagor.reis@gmail.com>
2024-09-07 02:02:27 -0300

    Imersão dev completa - segue o resultado final!

---
+++ dados.js
@@ -0,0 +1,34 @@
+let dados = [
+    {
+        titulo: "Paizada",
+    descricao: "Ser pai é uma jornada repleta de desafios e recompensas. É assumir o compromisso de criar, educar e proteger um novo ser humano, moldando seu caráter e oferecendo um ambiente seguro e amoroso para seu desenvolvimento. A paternidade exige dedicação, paciência e uma constante busca por aprendizado, pois cada filho é único e apresenta suas próprias necessidades. É um privilégio acompanhar o crescimento e as conquistas de um filho, mas também uma responsabilidade imensa de prepará-lo para enfrentar os desafios da vida.",
+    link: "https://youtu.be/EE1CJByA_lo?si=TyxCOlkX1fEhecII",
+    tags: "Paizada paizada paiz"
+
+},
+
+{
+    titulo: "Balaio de Pais",
+    descricao: "Podcast sobre a paternidade real, com histórias e dicas para pais de todas as idades.",
+    link: "https://open.spotify.com/episode/1RDdR0hGpS4RLVmPKuTJQA?si=vvC6qBOZSYym53nbze55GA&utm_source=copy-link&dl_branch=1&nd=1&dlsi=f33c5aa467174482",
+    tags: "balaio Balaio bala"
+
+},
+
+{
+    titulo: "AfroPai",
+    descricao: "Podcast que aborda a paternidade negra, com reflexões sobre raça, cultura e identidade.",
+    link: "https://open.spotify.com/show/4KgYdUKOktwlkOiCmDU4BJ",
+    tags: "afro AfroPai Afro"
+
+},
+
+{
+    titulo: "Ôpaiê",
+    descricao: "Podcast com bate-papos sobre paternidade, maternidade e família.", 
+    link: "https://open.spotify.com/episode/7iavQN9RKVaH6uti2PysN7",
+    tags: "Opaie opaie Ôpaiê ôpaiê"
+
+}
+
+];
